9627d98a12fb833e183246240234bed952c10874
ismael-martin <azarquiel.99693@gmail.com>
2020-12-07 13:39:50 +0100

    a

--- 06-bucles/while.py
+++ 06-bucles/while.py
@@ -20,3 +20,18 @@ while contador <= 100:
     texto = texto + ", " + str(contador)
     contador += 1
 print(texto)
+
+print("TABLA DE MULTIPLICAR CON WHILE")
+
+numero = int(input("De qué numero quieres la tabla?: "))
+contador = 1
+if numero < 1:
+    numero = 1
+
+print(f"TABLA DEL {numero}")
+
+while contador <= 10:
+    print(f"{numero} * {contador} = {numero * contador}")
+    contador += 1
+else:
+    print("TABLA TERMINADA")
