0db85e1d3110129621ab85b23c61114e3da6a287
ismael-martin <azarquiel.99693@gmail.com>
2020-12-07 13:49:40 +0100

    l

---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,19 @@
+"""
+CREAR DOS VARIABLES PAIS CONTINENTE Y MOSTRAR SU VALOR POR PANTALLA
+PONER COMENTARIO MOSTRANDO EL TIPO DE DATO
+"""
+
+pais = "España" #STRING
+continente = "Europa" #STRING
+year = 2020 #INT
+
+def mostrarDatoTipo(variable):
+    print(variable)
+    print(type(variable))
+    print("----------------")
+
+mostrarDatoTipo(pais)
+
+mostrarDatoTipo(continente)
+
+mostrarDatoTipo(year)
