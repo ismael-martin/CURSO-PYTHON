7e982922b923a5ebc8d57ef0b218077ca4bc39bb
ismael-martin <azarquiel.99693@gmail.com>
2020-12-07 17:06:47 +0100

    py

---
+++ 07-ejercicios/ejercicio4.py
@@ -0,0 +1,12 @@
+"""
+PEDIR DOS NUMEROS POR PANTALLA Y HACER TODAS LAS OPERACIONES BASICAS
+DE UNA CALCULADORA Y MOSTRARLAS POR PANTALLA
+"""
+
+num1 = int(input("Introduce tu primer numero: "))
+num2 = int(input("Introduce tu segundo numero: "))
+
+print(f"La suma de {num1} y {num2} es {num1 + num2}")
+print(f"La resta de {num1} y {num2} es {num1 - num2}")
+print(f"El producto de {num1} por {num2} es {num1 * num2}")
+print(f"La división de {num1} entre {num2} es {num1 / num2}")
