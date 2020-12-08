5ed08a445bc82c8a5de6ca2d5364df9abf97c26e
ismael-martin <azarquiel.99693@gmail.com>
2020-12-08 18:35:21 +0100

    py

---
+++ 12-modulos/moduloMatematicas.py
@@ -0,0 +1,15 @@
+import math
+
+print("Raiz cuadrada de 10: ", math.sqrt(10))
+
+print("Numero pi = ", math.pi)
+
+print("Redondear al alza: ", math.ceil(math.pi))
+print("Redondear a la baja: ", math.floor(math.pi))
+
+#numeros aleatorios
+import random
+print("Numero aleatorio entre el 15 y el 67: ", random.randint(15,67))
+print("Numero aleatorio entre el 15 y el 67: ", random.randint(15,67))
+print("Numero aleatorio entre el 15 y el 67: ", random.randint(15,67))
+print("Numero aleatorio entre el 15 y el 67: ", random.randint(15,67))
