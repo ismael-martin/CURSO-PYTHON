19bd042d25a62bca958ae7aaebfb1f8bf4a1a51b
ismael-martin <azarquiel.99693@gmail.com>
2020-12-08 18:49:18 +0100

    py

--- 13-paquetes/main.py
+++ 13-paquetes/main.py
@@ -2,7 +2,10 @@ print("PROBANDO PAQUETES")
 
 from mipaquete import pruebas
 from mipaquete import herramientas
+#tambien puedo hacer from mipaquete import pruebas, herramientas
 
 pruebas.probando()
 
 herramientas.concatNombreApellidos("Ismael", "Martín")
+
+#podemos instalar paquetes desde pypi.org
