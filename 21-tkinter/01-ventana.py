da7886abd4fb72d7e366be936bae2c526e800710
ismael-martin <azarquiel.99693@gmail.com>
2020-12-13 18:55:28 +0100

    13/12/2020 18:55

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -2,29 +2,58 @@
 tkinter es un modulo para crear interfaces graficas de usuario
 """
 from tkinter import *
-""" import os
-directorio = os.listdir()
-print(directorio) """
-#crear la ventana raiz
-ventana = Tk()
-
-#Titulo de la ventana
-ventana.title("Interfaz de la ventana")
-
-#icono de la ventana
-ventana.iconbitmap("/home/ismael/Documentos/PYTHON/CURSO PYTHON VICTOR ROBLES/21-tkinter/img/chrome.png")
+import os.path
 
 
-#cambiar el tamaño de la ventana
-ventana.geometry("750x450")
 
-#bloequer tamaño de la ventana
-ventana.resizable(0,0)
-"""
-son flags para bloquear el ancho y el alto
-0 para bloquear
-1 para desbloquear
-"""
+class Programa:
+    def __init__(self):
+        self.titulo = "Master en Python de Ismael Martín"
+        self.icon = "@./21-tkinter/img/favicon.xbm"
+        self.icon_alt = ""
+        self.size = "770x400"
+        self.resizable = True
+        self.ventana = Tk()
+    
+    def cargar(self):
+        #crear la ventana raiz
+        #Titulo de la ventana
+        self.ventana.title(self.titulo)
+        ruta_icono = os.path.abspath(self.icon)
+        #icono de la ventana
+        self.ventana.iconbitmap(self.icon)
+        #NO SE EXPLICA EN EL CURSO: en linux hay que poner @ segudo de la ruta (absoluta o relativa) del icono
+        #e iconbitmap solo reconoce archivos con formato xbm, mas adelante buscaré la forma de incluir cualquier icono en cualquier formato
 
-#arrancar y mostrar la ventana hasta que se cierra
-ventana.mainloop()
+        #mostrar texto en la ventana
+        texto = Label(self.ventana, text = ruta_icono)
+        texto.pack()
+
+        #cambiar el tamaño de la ventana
+        self.ventana.geometry(self.size)
+
+        #bloequer tamaño de la ventana
+        if self.resizable:
+            self.ventana.resizable(1,1)
+        else:
+            self.ventana.resizable(0,0)
+        #los paramoretros del metodo resizable son (ancho, alto)
+        """
+        son flags para bloquear el ancho y el alto
+        0 para bloquear
+        1 para desbloquear
+        """
+    def mostrarVentana(self):
+        #arrancar y mostrar la ventana hasta que se cierra
+        self.ventana.mainloop()
+
+    def addTexto(self, dato):
+        texto = Label(self.ventana, text = dato)
+        texto.pack()
+
+programa = Programa()
+programa.cargar()
+programa.addTexto("hola")
+programa.addTexto("que tal")
+programa.addTexto("me llamo ismael")
+programa.mostrarVentana()
